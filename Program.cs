672be478b4fa08a62fe84755dd43b5517e46b79a
Manon <manonlefkos@gmail.com>
2023-10-27 14:25:23 +0200

    clean Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
-﻿int[] inventory = { 200, 450, 700, 175, 250 };
-
-int sum = 0;
-int bin = 0;
-
-foreach (int items in inventory)
-{
-  bin ++;
-  sum += items;
-  Console.WriteLine($"Bin {bin} = {items} (Running total: {sum})");
-}
-Console.WriteLine($"We have {sum} items in inventory.");
+﻿
