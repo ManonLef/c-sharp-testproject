194812ceea20c6f42167d33780a1de862be87b1b
Manon <manonlefkos@gmail.com>
2023-10-30 22:24:08 +0100

    add permissions exercise

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿Random flip = new();
-int flipResult = flip.Next(0, 2);
- Console.WriteLine(flipResult == 0 ? "heads" : "tails");
+﻿string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin"))
+{
+  Console.WriteLine(level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user.");
+}
+else if (permission.Contains("Manager") && level >= 20)
+{
+  Console.WriteLine("Contact an Admin for access.");
+}
+else
+{
+  Console.WriteLine("You do not have sufficient privileges.");
+}
