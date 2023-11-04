ba13356b9e6f2dae2f4b05496f5e842e1d23a60a
Manon <manonlefkos@gmail.com>
2023-11-04 19:53:56 +0100

    finish scope and readability exercise module

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
 ﻿// variable scope and logic https://learn.microsoft.com/en-us/training/modules/csharp-code-blocks/2-exercise-variable-scope
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+bool found = false;
+int total = 0;
+
+foreach (int number in numbers)
+{
+  total += number;
+  
+  if (number == 42)
+    found = true;
+}
+
+if (found)
+  Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
