c9abb17eec3380bf67780f6d8f34b5e276b13384
Manon <manonlefkos@gmail.com>
2023-11-04 19:38:54 +0100

    clean slate

--- Program.cs
+++ Program.cs
@@ -1,15 +1,1 @@
-﻿string permission = "Admin|Manager";
-int level = 55;
-
-if (permission.Contains("Admin"))
-{
-  Console.WriteLine(level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user.");
-}
-else if (permission.Contains("Manager") && level >= 20)
-{
-  Console.WriteLine("Contact an Admin for access.");
-}
-else
-{
-  Console.WriteLine("You do not have sufficient privileges.");
-}
+﻿// variable scope and logic https://learn.microsoft.com/en-us/training/modules/csharp-code-blocks/2-exercise-variable-scope
