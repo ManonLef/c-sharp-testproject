4e8d1d7f0a83f04d58d392fe7c5e42f7e698bd5a
Manon <manonlefkos@gmail.com>
2023-11-07 20:52:42 +0100

    switch converted to statement

--- Program.cs
+++ Program.cs
@@ -3,21 +3,12 @@ string employeeName = "John Smith";
 
 string title = "";
 
-switch (employeeLevel)
+title = employeeLevel switch
 {
-    case 100:
-    case 200:
-        title = "Senior Associate";
-        break;
-    case 300:
-        title = "Manager";
-        break;
-    case 400:
-        title = "Senior Manager";
-        break;
-    default:
-        title = "Associate";
-        break;
-}
+    100 or 200 => "Senior Associate",
+    300 => "Manager",
+    400 => "Senior Manager",
+    _ => "Associate",
+};
 
 Console.WriteLine($"{employeeName}, {title}");
